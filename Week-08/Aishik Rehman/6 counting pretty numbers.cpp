489171f303724207cb7779d9fb7962174d30fa98
Aishik Rehman <aishikrehman@gmail.com>
2023-05-14 20:26:53 +0600

    Add files via upload

---
+++ Week-08/Aishik Rehman/6 counting pretty numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int l, r, i, j = 0, k;
+        cin >> l >> r;
+        for (i = l; i <= r; i++ ) {
+            k = i % 10;
+            if (k == 2 || k == 3 || k == 9) {
+                j++;
+            }
+        }
+        cout << j << endl;
+    }
+}
